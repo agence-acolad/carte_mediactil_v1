599495fb7dd3b354037d1760627710867376cff6
imTomah <colomas.thomas@orange.fr>
2021-02-22 12:25:30 +0100

    design page d'accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,18 +7,29 @@ use App\Entity\Produit;
 use App\Repository\CategorieRepository;
 use App\Repository\OngletRepository;
 use App\Repository\ProduitRepository;
+use Doctrine\DBAL\Tools\Dumper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
+    public $cat;
+
+
+    function __construct( CategorieRepository $repoC)
+    {
+        $this->cat      = $repoC->findAll();
+    }
     /**
      * @Route("/", name="accueil")
      */
-    public function index( ProduitRepository $repoProduit)
+    public function index( ProduitRepository $repoProduit, CategorieRepository $repoC)
     {
+        $categories = $repoC->findAll();
         return $this->render('index.html.twig', [
-            'produits' => $repoProduit->findAll()
+            'produits' => $repoProduit->findAll(),
+            'categories' => $categories,
+           
         ]);
     }
 
