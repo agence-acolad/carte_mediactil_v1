dc33379fc18c9f86e77b2a815aa6ef91946dd03b
imTomah <colomas.thomas@orange.fr>
2021-03-08 14:17:53 +0100

    probleme bdd réglés

--- migrations/Version20210308131416.php
+++ migrations/Version20210308131416.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
-use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210308125011 extends AbstractMigration
+final class Version20210308131416 extends AbstractMigration
 {
     public function getDescription() : string
     {
