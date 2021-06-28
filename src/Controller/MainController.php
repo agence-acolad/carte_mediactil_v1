8128b2200d852547878618c7d5007bed51081cff
matthieuveysset <57669271+matthieuveysset@users.noreply.github.com>
2021-06-28 15:51:47 +0200

    test

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -227,6 +227,176 @@ class MainController extends AbstractController
         'onglets' => $onglets,
        ]);
     }
+    
+    /**
+    * @Route("/nes", name="nes")
+    */
+    public function nes(ProduitRepository $repoProduit, CategorieRepository $repoC, OngletRepository $repoO)
+    {
+       $onglets = $repoO->findAll();
+       return $this->render('nespresso/nespresso.html.twig', [
+        'catNext' => $repoC->findAll(),
+        'produits' => $repoProduit->findAll(),
+        'catMenus' => $repoC->findOneBy(['nom' => 'Menus']),
+        'MenuEnt' => $repoC->findOneBy(['nom' => 'Menu - Entrées']),
+        'MenuPlat' => $repoC->findOneBy(['nom' => 'Menu - Plats']),
+        'MenuDess' => $repoC->findOneBy(['nom' => 'Menu - Desserts']),
+        'onglets' => $onglets,
+       ]);
+    }
+    
+    /**
+    * @Route("/nesRU", name="nesRU")
+    */
+    public function nesRU(ProduitRepository $repoProduit, CategorieRepository $repoC, OngletRepository $repoO)
+    {
+       $onglets = $repoO->findAll();
+       return $this->render('nespresso/nespresso-ru.html.twig', [
+        'catNext' => $repoC->findAll(),
+        'produits' => $repoProduit->findAll(),
+        'catMenus' => $repoC->findOneBy(['nom' => 'Menus']),
+        'MenuEnt' => $repoC->findOneBy(['nom' => 'Menu - Entrées']),
+        'MenuPlat' => $repoC->findOneBy(['nom' => 'Menu - Plats']),
+        'MenuDess' => $repoC->findOneBy(['nom' => 'Menu - Desserts']),
+        'onglets' => $onglets,
+       ]);
+    }
+    
+    /**
+    * @Route("/nesIT", name="nesIT")
+    */
+    public function nesIT(ProduitRepository $repoProduit, CategorieRepository $repoC, OngletRepository $repoO)
+    {
+       $onglets = $repoO->findAll();
+       return $this->render('nespresso/nespresso-it.html.twig', [
+        'catNext' => $repoC->findAll(),
+        'produits' => $repoProduit->findAll(),
+        'catMenus' => $repoC->findOneBy(['nom' => 'Menus']),
+        'MenuEnt' => $repoC->findOneBy(['nom' => 'Menu - Entrées']),
+        'MenuPlat' => $repoC->findOneBy(['nom' => 'Menu - Plats']),
+        'MenuDess' => $repoC->findOneBy(['nom' => 'Menu - Desserts']),
+        'onglets' => $onglets,
+       ]);
+    }
+    
+    /**
+    * @Route("/nesEN", name="nesEN")
+    */
+    public function nesEN(ProduitRepository $repoProduit, CategorieRepository $repoC, OngletRepository $repoO)
+    {
+       $onglets = $repoO->findAll();
+       return $this->render('nespresso/nespresso-en.html.twig', [
+        'catNext' => $repoC->findAll(),
+        'produits' => $repoProduit->findAll(),
+        'catMenus' => $repoC->findOneBy(['nom' => 'Menus']),
+        'MenuEnt' => $repoC->findOneBy(['nom' => 'Menu - Entrées']),
+        'MenuPlat' => $repoC->findOneBy(['nom' => 'Menu - Plats']),
+        'MenuDess' => $repoC->findOneBy(['nom' => 'Menu - Desserts']),
+        'onglets' => $onglets,
+       ]);
+    }
+    
+    /**
+    * @Route("/nesDE", name="nesDE")
+    */
+    public function nesDE(ProduitRepository $repoProduit, CategorieRepository $repoC, OngletRepository $repoO)
+    {
+       $onglets = $repoO->findAll();
+       return $this->render('nespresso/nespresso-de.html.twig', [
+        'catNext' => $repoC->findAll(),
+        'produits' => $repoProduit->findAll(),
+        'catMenus' => $repoC->findOneBy(['nom' => 'Menus']),
+        'MenuEnt' => $repoC->findOneBy(['nom' => 'Menu - Entrées']),
+        'MenuPlat' => $repoC->findOneBy(['nom' => 'Menu - Plats']),
+        'MenuDess' => $repoC->findOneBy(['nom' => 'Menu - Desserts']),
+        'onglets' => $onglets,
+       ]);
+    }
+    
+    /**
+    * @Route("/dej", name="dej")
+    */
+    public function dej(ProduitRepository $repoProduit, CategorieRepository $repoC, OngletRepository $repoO)
+    {
+       $onglets = $repoO->findAll();
+       return $this->render('petit_dejeuner/pdej.html.twig', [
+        'catNext' => $repoC->findAll(),
+        'produits' => $repoProduit->findAll(),
+        'catMenus' => $repoC->findOneBy(['nom' => 'Menus']),
+        'MenuEnt' => $repoC->findOneBy(['nom' => 'Menu - Entrées']),
+        'MenuPlat' => $repoC->findOneBy(['nom' => 'Menu - Plats']),
+        'MenuDess' => $repoC->findOneBy(['nom' => 'Menu - Desserts']),
+        'onglets' => $onglets,
+       ]);
+    }
+    
+    /**
+    * @Route("/dejRU", name="dejRU")
+    */
+    public function dejRU(ProduitRepository $repoProduit, CategorieRepository $repoC, OngletRepository $repoO)
+    {
+       $onglets = $repoO->findAll();
+       return $this->render('petit_dejeuner/pdej-ru.html.twig', [
+        'catNext' => $repoC->findAll(),
+        'produits' => $repoProduit->findAll(),
+        'catMenus' => $repoC->findOneBy(['nom' => 'Menus']),
+        'MenuEnt' => $repoC->findOneBy(['nom' => 'Menu - Entrées']),
+        'MenuPlat' => $repoC->findOneBy(['nom' => 'Menu - Plats']),
+        'MenuDess' => $repoC->findOneBy(['nom' => 'Menu - Desserts']),
+        'onglets' => $onglets,
+       ]);
+    }
+    
+    /**
+    * @Route("/dejEN", name="dejEN")
+    */
+    public function dejEN(ProduitRepository $repoProduit, CategorieRepository $repoC, OngletRepository $repoO)
+    {
+       $onglets = $repoO->findAll();
+       return $this->render('petit_dejeuner/pdej-en.html.twig', [
+        'catNext' => $repoC->findAll(),
+        'produits' => $repoProduit->findAll(),
+        'catMenus' => $repoC->findOneBy(['nom' => 'Menus']),
+        'MenuEnt' => $repoC->findOneBy(['nom' => 'Menu - Entrées']),
+        'MenuPlat' => $repoC->findOneBy(['nom' => 'Menu - Plats']),
+        'MenuDess' => $repoC->findOneBy(['nom' => 'Menu - Desserts']),
+        'onglets' => $onglets,
+       ]);
+    }
+    
+    /**
+    * @Route("/dejIT", name="dejIT")
+    */
+    public function dejIT(ProduitRepository $repoProduit, CategorieRepository $repoC, OngletRepository $repoO)
+    {
+       $onglets = $repoO->findAll();
+       return $this->render('petit_dejeuner/pdej-it.html.twig', [
+        'catNext' => $repoC->findAll(),
+        'produits' => $repoProduit->findAll(),
+        'catMenus' => $repoC->findOneBy(['nom' => 'Menus']),
+        'MenuEnt' => $repoC->findOneBy(['nom' => 'Menu - Entrées']),
+        'MenuPlat' => $repoC->findOneBy(['nom' => 'Menu - Plats']),
+        'MenuDess' => $repoC->findOneBy(['nom' => 'Menu - Desserts']),
+        'onglets' => $onglets,
+       ]);
+    }
+    
+    /**
+    * @Route("/dejDE", name="dejDE")
+    */
+    public function dejDE(ProduitRepository $repoProduit, CategorieRepository $repoC, OngletRepository $repoO)
+    {
+       $onglets = $repoO->findAll();
+       return $this->render('petit_dejeuner/pdej-de.html.twig', [
+        'catNext' => $repoC->findAll(),
+        'produits' => $repoProduit->findAll(),
+        'catMenus' => $repoC->findOneBy(['nom' => 'Menus']),
+        'MenuEnt' => $repoC->findOneBy(['nom' => 'Menu - Entrées']),
+        'MenuPlat' => $repoC->findOneBy(['nom' => 'Menu - Plats']),
+        'MenuDess' => $repoC->findOneBy(['nom' => 'Menu - Desserts']),
+        'onglets' => $onglets,
+       ]);
+    }
 
     /**
     * @Route("/aide", name="help")
